fded3d04c22cec193a16b28b9871d6d6212f3603
Amen Christ <amenphronesis@gmail.com>
2023-08-20 00:43:44 +0100

    Began outlining dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,8 +5,25 @@ function Dashboard() {
 
   const user = useMyStore((store) => store.user)
 
+  const style = {
+    width: '300px',
+    height: '300px',
+    border: '2px solid'
+  }
+
   return (
-    <div>Dashboard of {user?.email}</div>
+    <div >Dashboard of {user?.email}
+      <div style={{display: 'flex'}}>
+        <div style={style}><p>Tithes</p></div>
+        <div style={style}><p>Offerings</p></div>
+        <div style={style}><p>Partnership</p></div>
+      </div>
+      <div style={{width: '600px', height: '300px', border: '2px solid'}}>
+        <p>Giving Trend Graph</p>
+      </div>
+      <div style={style}><p>Recent Transactions</p></div>
+      <div style={style}><p>Notes</p></div>
+    </div>
   )
 }
 
